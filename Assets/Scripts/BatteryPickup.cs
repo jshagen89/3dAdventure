3c9d67937e1509ce70fe5757aecf2e31418b0410
jshagen <jshagen@tlu.edu>
2016-07-31 09:05:08 -0500

    first commit

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteryPickup : MonoBehaviour
+{
+    private float rotationSpeed = 0.1f;
+
+	private void Start()
+	{
+        StartCoroutine(Spin());
+	}
+
+    private IEnumerator Spin()
+    {
+        while (true)
+        {
+            transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed * 360);
+            yield return 0;
+        }
+    }
+}
